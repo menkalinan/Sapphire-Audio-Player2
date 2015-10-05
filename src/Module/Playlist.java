be68e1cbca0f83dfb85833cda9546f1ca15ef606
alexIvantsov <oleksandr.ivantsov@gmail.com>
2015-10-05 22:37:33 +0300

    added saving and loading playlist

--- src/Module/Playlist.java
+++ src/Module/Playlist.java
@@ -2,7 +2,11 @@ package Module;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -50,5 +54,40 @@ public class Playlist {
 
     public ArrayList<MyTrack> getMusicList(){
         return this.musicList;
+    }
+
+    public void savePlaylist() throws JSONException {
+        JSONArray jsonArray = new JSONArray();
+        for(MyTrack track: musicList){
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("artist",track.getArtist());
+            jsonObject.put("date", track.getDate());
+            jsonObject.put("duration", track.getDuration());
+            jsonObject.put("genre_id", track.getGenre_id());
+            jsonObject.put("lyrics_id", track.getLyrics_id());
+            jsonObject.put("title", track.getTitle());
+            jsonObject.put("url", track.getUrl());
+            jsonArray.put(jsonObject);
+        }
+        try {
+
+            FileWriter file = new FileWriter("my_playlist.json");
+            file.write(jsonArray.toString());
+            file.flush();
+            file.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadPlaylist() throws IOException {
+        FileReader file = new FileReader("my_playlist.json");
+        String result = "";
+        int c;
+        while((c=file.read())!=-1){
+            result += Character.toString((char)c);
+        }
+        parseJSON(result);
     }
 }
