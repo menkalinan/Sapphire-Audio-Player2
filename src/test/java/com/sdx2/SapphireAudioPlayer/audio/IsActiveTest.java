f3ba077cd53cc7c524908e3fdee39eb8a8a05981
Dima Havrish <profesor.nevri@gmail.com>
2015-12-01 01:57:44 +0200

    Tests update.

--- src/test/java/com/sdx2/SapphireAudioPlayer/audio/IsActiveTest.java
+++ src/test/java/com/sdx2/SapphireAudioPlayer/audio/IsActiveTest.java
@@ -1,13 +1,16 @@
 package test.java.com.sdx2.SapphireAudioPlayer.audio;
+import main.java.com.sdx2.SapphireAudioPlayer.audio.PlayThread;
 import main.java.com.sdx2.SapphireAudioPlayer.audio.Player;
+import main.java.com.sdx2.SapphireAudioPlayer.audio.data.TrackBuffer;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class IsPlayingTest {
+public class IsActiveTest {
     @Test
     public void isPlayingTest(){
         Player pl = new Player();
-        assertThat(pl.isPlaying());
+        PlayThread pt = new PlayThread(pl,new TrackBuffer());
+        assertThat(pt.isActive()).isFalse();
     }
 }
